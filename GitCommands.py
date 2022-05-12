9c0c808c14b670beb25bd70ae5aae7629db6b1d2
Matt Cabe <mcabe@layer3com.com>
2022-05-12 00:04:51 +0000

    2022-05-12

--- GitCommands.py
+++ GitCommands.py
@@ -1,4 +1,4 @@
-# This module runs git commands to updated generated folder after pull
+# This module runs git commands to updated generated folder after polling SNMP
 
 import subprocess
 import datetime
@@ -7,15 +7,20 @@ def run():
 
     # Get current date
     timestamp = datetime.datetime.now()
-    timestamp = str(timestamp.strftime('%Y-%m-%d') )
+    timestamp = str(timestamp.strftime('%Y-%m-%d'))
     
     # Run Git commands to upload previous
+    # Run git add, though there should not be any changes
     p = subprocess.Popen(['git', 'add', '*'], cwd = "/home/mcabe/Obsidian", stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     out, err = p.communicate()
     data = str(out)
+    
+    #Run git commit with time current date as timestamp
     p = subprocess.Popen(['git', 'commit', '-m', timestamp], cwd = "/home/mcabe/Obsidian", stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     out, err = p.communicate()
     data = str(out)
+    
+    # Run git push to update file in Github
     p = subprocess.Popen(['git', 'push'], cwd = "/home/mcabe/Obsidian", stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     out, err = p.communicate()
     data = str(out)
